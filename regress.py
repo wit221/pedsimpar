5582977053b4b551e0f342ccfcda07ac4bb2d1fb
Samuel Kim <ubuntor2000@gmail.com>
2018-04-16 21:17:30 -0400

    add speedup

--- regress.py
+++ regress.py
@@ -7,9 +7,11 @@ import time
 print("[.] making libraries")
 subprocess.check_output(["make"], cwd="./libpedsim")
 subprocess.check_output(["make"], cwd="./libpedsimpar")
+
 print("[.] making test executables")
 subprocess.check_output("g++ ./regress/regress.cpp -o regress/regress_seq -Ilibpedsim -lpedsim -Llibpedsim -g -std=c++0x", shell=True)
 subprocess.check_output("g++ ./regress/regress.cpp -o regress/regress_par -Ilibpedsimpar -lpedsimpar -Llibpedsimpar -g -std=c++0x", shell=True)
+
 print("[.] running sequential")
 start = time.time()
 subprocess.check_output("LD_LIBRARY_PATH=./libpedsim/:${LD_LIBRARY_PATH} ./regress/regress_seq", shell=True)
@@ -17,6 +19,7 @@ subprocess.check_output("mv ./pedsim_out.txt ./pedsim_out_seq.txt", shell=True)
 end = time.time()
 seqtime = end-start
 print("Sequential: {} ms".format(seqtime*1000.0))
+
 print("[.] running parallel")
 start = time.time()
 subprocess.check_output("LD_LIBRARY_PATH=./libpedsimpar/:${LD_LIBRARY_PATH} ./regress/regress_par", shell=True)
@@ -24,6 +27,8 @@ subprocess.check_output("mv ./pedsim_out.txt ./pedsim_out_par.txt", shell=True)
 end = time.time()
 partime = end-start
 print("Parallel:   {} ms".format(partime*1000.0))
+print("Speedup: {}".fomrat(seqtime/partime))
+
 print("[.] checking output")
 
 outputSeq = open("./pedsim_out_seq.txt").read().strip().splitlines()
