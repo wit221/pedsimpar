b80372bada680221df157e1a613241bf4b32ac88
Samuel Kim <sdkim1@andrew.cmu.edu>
2018-05-08 00:20:26 -0400

    regress2

---
+++ regress/regress2.cpp
@@ -0,0 +1,145 @@
+//
+// pedsim - A microscopic pedestrian simulation system.
+// Copyright (c) by Christian Gloor
+//
+// To collect the output in a file::
+// ./example05 > out.dat
+//
+// Process output in gnuplot:
+// gnuplot> plot "out.dat"
+
+#include "ped_includes.h"
+
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+
+using namespace std;
+
+void simulate(int numagents, int maxtime, bool quadtree) {
+    // setup
+    Ped::Tscene *pedscene;
+
+    if (quadtree) {
+        pedscene = new Ped::Tscene(-200, -200, 400, 400);
+    } else {
+        pedscene = new Ped::Tscene(); // no quadtree
+    }
+
+    // create an output writer which will send output to a visualizer
+    //Ped::OutputWriter *ow = new Ped::FileOutputWriter();
+    //Ped::OutputWriter *ow = new Ped::UDPOutputWriter();
+    //ow->setScenarioName("Regression/Benchmark Test");
+    //pedscene->setOutputWriter(ow);
+
+    // create and add obstacles
+    pedscene->addObstacle(new Ped::Tobstacle(-100, -50, 100, -50));
+    pedscene->addObstacle(new Ped::Tobstacle(-100, 50, 100, 50));
+    pedscene->addObstacle(new Ped::Tobstacle(-100, -50, -100, 50));
+    pedscene->addObstacle(new Ped::Tobstacle(100, -50, 100, 50));
+
+    // dynamic obstacles
+    Ped::Tobstacle *do1 = new Ped::Tobstacle(-40, -5, 40, -5);
+    pedscene->addObstacle(do1);
+    Ped::Tobstacle *do2 = new Ped::Tobstacle(-40, -5, -100, -50);
+    pedscene->addObstacle(do2);
+    Ped::Tobstacle *do3 = new Ped::Tobstacle(40, -5, 100, -50);
+    pedscene->addObstacle(do3);
+    Ped::Tobstacle *do4 = new Ped::Tobstacle(-40, 5, 40, 5);
+    pedscene->addObstacle(do4);
+    Ped::Tobstacle *do5 = new Ped::Tobstacle(-40, 5, -100, 50);
+    pedscene->addObstacle(do5);
+    Ped::Tobstacle *do6 = new Ped::Tobstacle(40, 5, 100, 50);
+    pedscene->addObstacle(do6);
+
+    // add one waypoint (=destination) with a small radius of 10 at the right
+    // end.
+    Ped::Twaypoint *w1 = new Ped::Twaypoint(100, 0, 20);
+    pedscene->addWaypoint(w1);
+    Ped::Twaypoint *w2 = new Ped::Twaypoint(-100, 0, 20);
+    pedscene->addWaypoint(w2);
+
+    // create agents
+    for (int i = 0; i < numagents; i++) {
+        Ped::Tagent *a = new Ped::Tagent();
+        a->setWaypointBehavior(Ped::Tagent::BEHAVIOR_ONCE); // only once
+        a->setVmax(1.2); // same speed for all agents
+        a->setfactorsocialforce(10.0);
+        a->setfactorobstacleforce(2.0);
+        if (a->getid() % 2 == 0) {
+            a->setPosition(-80 - (a->getid() / 5000), -25 + (a->getid() % 5000)/100.0, 0);
+            a->addWaypoint(w1);
+        } else {
+            a->setPosition(80 + (a->getid() / 5000), -25 + (a->getid() % 5000)/100.0, 0);
+            a->addWaypoint(w2);
+        }
+        pedscene->addAgent(a);
+    }
+
+    long int timestep = 0;
+
+    auto myagents = pedscene->getAllAgents();
+
+    int notreached = myagents.size();
+    while (notreached > 0) {
+        timestep++;
+        notreached = myagents.size();
+        pedscene->moveAgents(0.4);
+        for (auto a : myagents) {
+            if (a->reachedDestination())
+                notreached--;
+        }
+        if (timestep >= maxtime)
+            notreached = 0; // seems to run forever.
+    }
+
+    // cleanup
+    for (auto a : pedscene->getAllAgents()) {
+        delete a;
+    };
+    for (auto w : pedscene->getAllWaypoints()) {
+        delete w;
+    };
+    for (auto o : pedscene->getAllObstacles()) {
+        delete o;
+    };
+    delete pedscene;
+}
+
+void usage(char* argv0) {
+    cout << argv0 << " [-n numagents]" << endl;
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    int numagents = 100;
+    int maxtime = 20000;
+    int c;
+    bool quadtree = false;
+    while ((c = getopt(argc, argv, "hn:t:q")) != -1) {
+        switch(c) {
+            case 'h':
+                usage(argv[0]);
+                break;
+            case 'n':
+                numagents = atoi(optarg);
+                break;
+            case 'q':
+                quadtree = true;
+                break;
+            case 't':
+                maxtime = atoi(optarg);
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (numagents <= 0 || numagents > 100000) {
+        cout << "Invalid number of agents (0 < numagents <= 100000)" << endl;
+        exit(0);
+    }
+
+    simulate(numagents, maxtime, quadtree);
+    return EXIT_SUCCESS;
+}
---
+++ regress2.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python2
+
+import difflib
+import subprocess
+import time
+import getopt
+import sys
+import os
+
+args = lambda x: None
+args.n = 100
+args.t = 20000
+args.q = False
+
+def usage():
+    print "Usage: regress.py [-h|--help] [-n NUMAGENTS] [-t MAXTIME] [-q]"
+    print "    -h --help    : Print usage."
+    print "    -n NUMAGENTS : Number of agents (0 < NUMAGENTS <= 100000)."
+    print "    -t MAXTIME   : Maximum number of timesteps."
+    print "    -q           : Use quadtree."
+
+try:
+    opts, _ = getopt.getopt(sys.argv[1:], "hn:t:q", ["help"])
+except getopt.GetoptError as err:
+    print(str(err))
+    usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o in ["-h", "--help"]:
+        usage()
+        sys.exit()
+    elif o == '-n':
+        args.n = int(a)
+    elif o == '-t':
+        args.t = int(a)
+    elif o == '-q':
+        args.q = True
+
+if args.n <= 0 or args.n > 100000:
+    print("[-] Invalid number of agents (0 < n <= 100000)")
+    sys.exit(0)
+
+print("[.] making libraries")
+subprocess.check_call(["make"], cwd="./libpedsim")
+subprocess.check_call(["make"], cwd="./libpedsimpar")
+
+print("[.] making test executables")
+subprocess.check_call("g++ ./regress/regress.cpp -o regress/regress_seq -Ilibpedsim -lpedsim -Llibpedsim -g -std=c++0x", shell=True)
+if os.path.exists("/opt/cuda-8.0/"):
+    cudalib = "-L/opt/cuda-8.0/lib64/ -lcudart"
+    cudalibdir = "/opt/cuda-8.0/lib64/"
+else:
+    cudalib = "-L/usr/local/depot/cuda-8.0/lib64/ -lcudart"
+    cudalibdir = "/usr/local/depot/cuda-8.0/lib64/"
+subprocess.check_call("g++ ./regress/regress.cpp -o regress/regress_par -Ilibpedsimpar -lpedsimpar -Llibpedsimpar {0} -g -std=c++0x".format(cudalib), shell=True)
+
+print("[.] running parallel")
+start = time.time()
+subprocess.check_call("LD_LIBRARY_PATH={3}:./libpedsimpar/:${{LD_LIBRARY_PATH}} ./regress/regress_par -n {0} -t {1} {2}".format(args.n, args.t, "-q" if args.q else "", cudalibdir), shell=True)
+#subprocess.check_call("mv ./pedsim_out.txt ./pedsim_out_par.txt", shell=True)
+end = time.time()
+partime = end-start
+print("Parallel:   {0} ms".format(partime*1000.0))
+
+print("[.] running sequential")
+start = time.time()
+subprocess.check_call("LD_LIBRARY_PATH=./libpedsim/:${{LD_LIBRARY_PATH}} ./regress/regress_seq -n {0} -t {1} {2}".format(args.n, args.t, "-q" if args.q else ""), shell=True)
+#subprocess.check_call("mv ./pedsim_out.txt ./pedsim_out_seq.txt", shell=True)
+end = time.time()
+seqtime = end-start
+print("Sequential: {0} ms".format(seqtime*1000.0))
+
+print("Speedup: {0}".format(seqtime/partime))
+'''
+print("[.] checking output")
+
+outputSeq = open("./pedsim_out_seq.txt").read().strip().splitlines()
+outputPar = open("./pedsim_out_par.txt").read().strip().splitlines()
+
+if outputSeq != outputPar:
+    print("[-] MISMATCH:")
+    for line in difflib.unified_diff(outputSeq, outputPar, lineterm=''):
+        print(line)
+else:
+    print("[+] Passed.")
+'''
